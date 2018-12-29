aa341d26f33961e93920d303e1c33f0112ca262b
Mark Ioannidis <mark.ioannidis@gmail.com>
2018-12-29 15:14:35 -0500

    updating day of details

--- src/containers/details.js
+++ src/containers/details.js
@@ -58,9 +58,9 @@ export default class Details extends Component {
 const additionalDetails = <div className="details-expanded"wrap="virtual"><b>
                           Both ceremony and reception will be held at the John James Audubon Center in Audubon, PA.
                           <ul>
-                          <u>Ceremony</u>
+                          <u>Day-of Details</u>
                           <li>September 28, 2019</li>
-                          <li>Start Time: 5:15 PM</li>
+                          <li>Ceremony Start Time: 5:15 PM</li>
                           <li>Cocktail Hour: 6:00 PM - 7:00 PM</li>
                           <li>Reception: 7:00 - 11:00 PM</li>
                           </ul>
@@ -74,7 +74,7 @@ const additionalDetails = <div className="details-expanded"wrap="virtual"><b>
                           <li>Time: After the reception</li>
                           </ul>
                           <ul><u>Transportation</u>
-                          <li>There will be a shuttle running from The Desmond Hotel to the ceremony location
+                          <li>There will be shuttles running from The Desmond Hotel to the ceremony location
                           and back after the reception ends. Times are TBD.</li>
                           </ul></b>
                           </div>;
